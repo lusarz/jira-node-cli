1382e9573eb00f279a5d0357927b758e5edf9f0e
lukasz <lukasz.usarz@gmail.com>
2018-02-28 21:55:44 +0100

    REFACTORING: rename issues to jql

--- test/utils/StringUtils.spec.js
+++ test/utils/StringUtils.spec.js
@@ -5,4 +5,8 @@ describe('StringUtils.fillWithSpaces', function () {
   it('return properly formatted text', function () {
     expect(StringUtils.fillWithSpaces('x', 5)).to.equal('x    ');
   });
+
+  it('return original text when is longer that required length', function () {
+    expect(StringUtils.fillWithSpaces('abcdefgh', 5)).to.equal('abcdefgh');
+  });
 });
