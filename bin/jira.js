9a3900f70082affc36dfd02639d8c704dcccfd6d
lukasz <lukasz.usarz@gmail.com>
2018-03-07 11:18:30 +0100

    Ask for host and credentials when configuration file doesn't exists yet.

--- bin/jira.js
+++ bin/jira.js
@@ -6,31 +6,48 @@ const ActionUtils = require('../lib/utils/ActionUtils');
 const ConfigurationUtils = require('../lib/utils/ConfigurationUtils');
 
 if (!ConfigurationUtils.configurationFileExists()) {
-  console.log('Please create \'.jirarc\' configuration file in your home directory before run');
-  process.exit();
+  ConfigurationUtils
+    .promptUserForConfigurationProperties()
+    .then(ConfigurationUtils.saveConfiguration)
+    .then(runProgram);
+} else {
+  runProgram();
 }
 
-RequireUtils
-  .readAvailableActions()
-  .forEach(action => {
-    const { print, fetch, props, description } = RequireUtils.getAction(action);
+function runProgram () {
+  RequireUtils
+    .readAvailableActions()
+    .forEach(action => {
+      const { print, fetch, props, description } = RequireUtils.getAction(action);
 
-    program
-      .command(ActionUtils.buildCommand(action, props))
-      .description(description)
-      .action(params => fetch(params).then(print).catch(err => { console.log(err); }))
-  });
+      program
+        .command(ActionUtils.buildCommand(action, props))
+        .description(description)
+        .action(params =>
+          fetch(params)
+            .then(print)
+            .catch(err => { console.log(err); })
+        );
+    });
 
-RequireUtils
-  .readAvailableAliases()
-  .forEach(alias => {
-    const { action, propsData, description } = RequireUtils.getAlias(alias);
-    const { print, fetch, props } = RequireUtils.getAction(action);
+  RequireUtils
+    .readAvailableAliases()
+    .forEach(alias => {
+      const { action, propsData, description } = RequireUtils.getAlias(alias);
+      const { print, fetch, props } = RequireUtils.getAction(action);
 
-    program
-      .command(alias)
-      .description(description)
-      .action(() => fetch(props.map(prop => propsData[prop])).then(print).catch(err => { console.log(err); }))
-  });
+      program
+        .command(alias)
+        .description(description)
+        .action(() =>
+          fetch(props.map(prop => propsData[prop]))
+            .then(print)
+            .catch(err => { console.log(err); })
+        );
+    });
 
-program.parse(process.argv);
+  program.parse(process.argv);
+  if (!program.args.length) {
+    program.help();
+  }
+}
