b79876faea52125c64cf0fca418c4099a1581510
lukasz <lukasz.usarz@gmail.com>
2018-10-31 08:47:53 +0100

    Use async/await

--- types/IssueDetails.d.ts
+++ types/IssueDetails.d.ts
@@ -3,4 +3,6 @@ import Issue from "./Issue";
 export default interface IssueDetails extends Issue {
     commentsCount: number;
     comments: Array<Comment>;
+    children: Array<IssueDetails>;
+    description: string;
 };
